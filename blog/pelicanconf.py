0c8d3e0d226844ee1a00712bbe16c2b00229b1e1
Jericho Rivera <jerichorivera@gmail.com>
2023-10-19 10:01:02 +0800

    Edit first post

--- blog/pelicanconf.py
+++ blog/pelicanconf.py
@@ -41,5 +41,15 @@ TYPOGRIPHY = True
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = ['readtime', 'neighbors']
 
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight'},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+        'markdown.extensions.sane_lists': {},
+        'markdown.extensions.admonition': {},
+    },
+    'output_format': 'html5',
+}
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
